fd89a74e5c34cbae0e8613d03c6d14f88a23d4da
welingtonfr <welingtont@hotmail.com>
2020-10-05 15:20:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const { env } = require('process')
+const app = express()
+const images = require('./imagesColections')
+
+app.get('/flags/:country', (req, res) => {
+    var options = {
+        root: path.join(__dirname, 'flags'),
+    }
+   res.sendFile(`${req.params.country}.png`,options)
+})
+
+
+app.listen(3000 || env.port , () => {
+    console.log('Servidor rodando!')
+})
