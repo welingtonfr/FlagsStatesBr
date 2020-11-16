522365d93c5cf2b75e70463bd5e3183c9b568d2f
Welington Ferreira <welingtont@hotmail.com>
2020-11-16 14:58:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const { env } = require('process')
 const app = express()
-const images = require('./imagesColections')
 
 app.get('/flags/:country', (req, res) => {
     var options = {
